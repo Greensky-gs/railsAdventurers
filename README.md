5df8acb6ab2ec4f2513a87c52593d34559d17cef
Greensky <david.hesliere@gmail.com>
2023-08-09 18:28:40 +0200

    chore: :tada: Start project

---
+++ README.md
@@ -0,0 +1,3 @@
+# RailsAdventurers
+
+It is a Discord bot (in french for now) that allow you to play to the game [Rails Adventurers, scandinavian edition](https://www.daysofwonder.com/tickettoride/fr/nordic/)
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { AmethystClient } from "amethystjs";
+import { config } from 'dotenv'
+
+config();
+
+export const client = new AmethystClient({
+    intents: ['Guilds']
+}, {
+    token: process.env.token,
+    commandsFolder: './dist/commands',
+    eventsFolder: './dist/events',
+    preconditionsFolder: './dist/preconditions',
+    autocompleteListenersFolder: './dist/autocompletes',
+    debug: true
+})
+
+client.start({});
